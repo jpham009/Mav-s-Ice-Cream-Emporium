1a6cffc0879e8549a3f235aa1b1b0b8f2651bcdb
Professor Rice <george.rice@uta.edu>
2017-10-31 22:54:08 -0500

    refactor mainwin to separate files, fix invalid data bug

---
+++ mainwin-create_serving.cpp
@@ -0,0 +1,26 @@
+#include "mainwin.h"
+#include <exception>
+#include <stdexcept>
+
+Mice::Serving Mainwin::create_serving() {
+    int container = select_container();
+    if (container == -1) throw std::runtime_error("Canceled");
+
+    Mice::Serving serving{_containers[container]};
+
+    bool has_no_scoops = true;
+    for (int i=0; i<_containers[container].max_scoops(); ++i) {
+        int scoop = select_scoop();
+        if (scoop == -1) break;
+        serving.add_scoop(_scoops[i]);
+        has_no_scoops = false;
+    }
+    if (has_no_scoops) throw std::runtime_error("Canceled");
+
+    while (true) {
+        int topping = select_topping();
+        if (topping = -1) break;
+        else serving.add_topping(_toppings[topping]);
+    }
+    return serving;
+}
